1811890d0f32f84c07a4dbbb5ae1a6de165d8224
David Craven <david@craven.ch>
2020-05-28 16:17:04 +0200

    Update client.

--- client/src/sunshine.rs
+++ client/src/sunshine.rs
@@ -1,59 +1,54 @@
 use crate::error::Error;
 #[cfg(feature = "light-client")]
 use crate::light_client::ChainType;
-use crate::runtime::{Client, Pair, Runtime, XtBuilder};
+use crate::runtime::{Client, Pair, PairSigner, Runtime};
 use crate::srml::shares_atomic::*;
 use ipfs_embed::{Config, Store};
 use ipld_block_builder::BlockBuilder;
-use sp_core::Pair as _;
 use std::path::Path;
-use substrate_subxt::system::*;
+use substrate_subxt::Signer;
 
 pub struct Sunshine {
-    account_id: <Runtime as System>::AccountId,
+    signer: PairSigner,
     subxt: Client,
-    xt: XtBuilder,
     ipld: BlockBuilder<Store>,
 }
 
 impl Sunshine {
+    fn create_block_builder(db: &sled::Db) -> Result<BlockBuilder<Store>, Error> {
+        let ipld_tree = db.open_tree("ipld_tree")?;
+        let config = Config::from_tree(ipld_tree);
+        let store = Store::new(config)?;
+        Ok(BlockBuilder::new(store))
+    }
+
     #[cfg(feature = "light-client")]
     pub async fn new<T: AsRef<Path>>(
         path: T,
-        signer: Pair,
+        pair: Pair,
         chain: ChainType,
     ) -> Result<Self, Error> {
         let db = sled::open(path)?;
-        let ipld_tree = db.open_tree("ipld_tree")?;
         let subxt_tree = db.open_tree("subxt_tree")?;
-        let config = Config::from_tree(ipld_tree);
-        let store = Store::new(config)?;
-        let ipld = BlockBuilder::new(store);
-        let account_id = signer.public().into();
         let subxt = crate::light_client::build_light_client(subxt_tree, chain).await?;
-        let xt = subxt.xt(signer, None).await?;
+        let signer = PairSigner::new(pair);
+        let ipld = Self::create_block_builder(&db)?;
         Ok(Self {
-            account_id,
             subxt,
-            xt,
+            signer,
             ipld,
         })
     }
 
     #[cfg(not(feature = "light-client"))]
-    pub async fn new<T: AsRef<Path>>(path: T, signer: Pair) -> Result<Self, Error> {
+    pub async fn new<T: AsRef<Path>>(path: T, pair: Pair) -> Result<Self, Error> {
         let db = sled::open(path)?;
-        let ipld_tree = db.open_tree("ipld_tree")?;
-        let config = Config::from_tree(ipld_tree);
-        let store = Store::new(config)?;
-        let ipld = BlockBuilder::new(store);
-        let account_id = signer.public().into();
+        let signer = PairSigner::new(pair);
         let subxt = crate::runtime::ClientBuilder::new().build().await?;
-        let xt = subxt.xt(signer, None).await?;
+        let ipld = Self::create_block_builder(&db)?;
         Ok(Self {
-            account_id,
+            signer,
             subxt,
-            xt,
             ipld,
         })
     }
@@ -63,10 +58,8 @@ impl Sunshine {
         org: u32,
         share: u32,
     ) -> Result<SharesReservedEvent<Runtime>, Error> {
-        self.xt
-            .clone()
-            .watch()
-            .reserve_shares(org, share, &self.account_id)
+        self.subxt
+            .reserve_shares_and_watch(&self.signer, org, share, self.signer.account_id())
             .await?
             .shares_reserved()
             .map_err(|e| substrate_subxt::Error::Codec(e))?
